fd830e8d9c7258771d1203e4da940d8e22f840c9
Thomas-Gallusser <t.gallusser@codeur.online>
2019-09-25 13:02:58 +0200

    move css

--- views/header.php
+++ views/header.php
@@ -6,8 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>StartupRR</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="progressBar.css">
+    <link rel="stylesheet" href="css/workForm.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/progressBar.css">
 </head>
 
 <body>
