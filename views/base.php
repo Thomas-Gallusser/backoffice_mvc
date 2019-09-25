0e972d6fe089c1b0707904e2b4065518f115d8cb
Thomas-Gallusser <t.gallusser@codeur.online>
2019-09-25 14:37:07 +0200

    pull

--- views/base.php
+++ views/base.php
@@ -358,30 +358,35 @@
 
                      echo '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-2 portfolio">
                             <div class="container-fluid hov">
-                            <div class="row iconsPortf">
-                              <div class="pos test">
-                              <a href="#sample">
-                                <img src="img/backicon.png">
-                                <div class="centered fas fa-link"></div>
-                              </a>
+
+                              <div class="row iconsPortf">
+                                <div class="pos test">
+                                  <a href="#sample">
+                                    <img src="img/backicon.png">
+                                    <div class="centered fas fa-link"></div>
+                                  </a>
+                                </div>
+
+                                <div class="pos search">
+                                  <a href="#sample">
+                                    <img src="img/backicon.png">
+                                    <div class="centered fas fa-search"></div>
+                                  </a>
+                                </div>
+
                               </div>
-                              <div class="pos search">
-                              <a href="#sample">
-                                <img src="img/backicon.png">
-                                <div class="centered fas fa-search"></div>
-                              </a>
+
+                              <div class="row infosPortf">
+                                <div class="col-12">
+                                  <p class="bold">' . $article->getNom() . '</p>
+                                  <p>' . $article->getGroupe() . ' / ' . $article->getType() . '</p>
+                                  <p>♥ ' . $article->getLikes() . '</p>
+                                </div>
                               </div>
-                            </div>
-                            <div class="row infosPortf">
-                              <div class="col-12">
-                              <p class="bold">' . $article->getNom() . '</p>
-                              <p>' . $article->getGroupe() . ' / ' . $article->getType() . '</p>
-                              <p>♥ ' . $article->getLikes() . '</p>
-                              </div>
-                            </div>
+                              
                             </div>
                           </div>';
-                          
+
                    ?>
 
                </div>
