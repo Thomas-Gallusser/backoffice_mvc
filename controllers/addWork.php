02e134443b5428479948746c15b55901711644d0
Thomas-Gallusser <t.gallusser@codeur.online>
2019-09-25 16:03:19 +0200

    add article not finish

--- controllers/addWork.php
+++ controllers/addWork.php
@@ -1,12 +1,17 @@
 <?php
+  require('../conf/settings.php');
+  require('../lib/database.lib.php');
+  require('../models/work.php');
 
   if (isset($_POST['title']) && isset($_POST['commentary']) && isset($_POST['groupe']) && isset($_POST['type']) && isset($_FILES['img'])) {
 
     $file = $_FILES["img"];
     if (strpos($file["type"], 'image/') !== false){
-      move_uploaded_file($file['tmp_name'], '../img/' . uniqid("upld_") . '.' . pathinfo($file["name"],PATHINFO_EXTENSION));
+      $imgId = uniqid("upld_") . '.' . pathinfo($file["name"],PATHINFO_EXTENSION);
+      move_uploaded_file($file['tmp_name'], '../img/' . $imgId);
 
-      
+      $newArticle = new Work(0, $_POST['title'], $_POST['groupe'], $_POST['type'], 0, $imgId, $_POST['commentary']);
+      $newArticle->create();
     }
 
   }
