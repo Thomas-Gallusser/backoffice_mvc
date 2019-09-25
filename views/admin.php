3a74dd032f09dc3426e91568e0f51cbe1a6d4006
Noled <e.morlon@codeur.online>
2019-09-25 11:36:10 +0200

    fix index

---
+++ views/admin.php
@@ -0,0 +1,6 @@
+<div class="h4">Connexion au Backoffice</div>
+
+<div class="position-absolute bg-light rounded p-3 border">
+  <div><input type="text" name="login" placeholder="Name" class="form-control rounded" /></div>
+  <div><input type="password" name="password" placeholder="password" class="form-control rounded mt-2" /></div>
+</div>
