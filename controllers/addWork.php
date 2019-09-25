86e8ac21716975d9371a18a5c0f1c4af5eeb8521
Thomas-Gallusser <t.gallusser@codeur.online>
2019-09-25 15:43:37 +0200

    prepare form article

--- controllers/addWork.php
+++ controllers/addWork.php
@@ -1,7 +1,14 @@
 <?php
 
-  if (isset($_POST['title']) && isset($_POST['commentary'])) {
-    
+  if (isset($_POST['title']) && isset($_POST['commentary']) && isset($_POST['groupe']) && isset($_POST['type']) && isset($_FILES['img'])) {
+
+    $file = $_FILES["img"];
+    if (strpos($file["type"], 'image/') !== false){
+      move_uploaded_file($file['tmp_name'], '../img/' . uniqid("upld_") . '.' . pathinfo($file["name"],PATHINFO_EXTENSION));
+
+      
+    }
+
   }
 
 ?>
