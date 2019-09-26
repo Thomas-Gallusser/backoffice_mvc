7e6f69afb28a3469d37939234ac67ff0faae93e3
Thomas-Gallusser <t.gallusser@codeur.online>
2019-09-26 20:55:38 +0200

    add image article

--- views/base.php
+++ views/base.php
@@ -349,35 +349,37 @@
             $articles = Work::getPartI(12,($_GET['p'] - 1) * 12);
 
             foreach($articles as $article){
-            echo '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-2 portfolio">
-                   <div class="container-fluid hov">
+            echo '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-2 portfolio" style="background-image: url(img/uploads/'.$article->getImage().');">
+                    <div class="col-12 portBac">
+                     <div class="container-fluid hov">
 
-                     <div class="row iconsPortf">
-                       <div class="pos test">
-                         <a href="#sample">
-                           <img src="img/backicon.png">
-                           <div class="centered fas fa-link"></div>
-                         </a>
+                       <div class="row iconsPortf">
+                         <div class="pos test">
+                           <a href="#sample">
+                             <img src="img/backicon.png">
+                             <div class="centered fas fa-link"></div>
+                           </a>
+                         </div>
+
+                         <div class="pos search">
+                           <a href="#sample">
+                             <img src="img/backicon.png">
+                             <div class="centered fas fa-search"></div>
+                           </a>
+                         </div>
+
                        </div>
 
-                       <div class="pos search">
-                         <a href="#sample">
-                           <img src="img/backicon.png">
-                           <div class="centered fas fa-search"></div>
-                         </a>
+                       <div class="row infosPortf">
+                         <div class="col-12">
+                           <p class="bold">' . $article->getNom() . '</p>
+                           <p>' . $article->getGroupe() . ' / ' . $article->getType() . '</p>
+                           <p>♥ ' . $article->getLikes() . '</p>
+                         </div>
                        </div>
 
-                     </div>
-
-                     <div class="row infosPortf">
-                       <div class="col-12">
-                         <p class="bold">' . $article->getNom() . '</p>
-                         <p>' . $article->getGroupe() . ' / ' . $article->getType() . '</p>
-                         <p>♥ ' . $article->getLikes() . '</p>
-                       </div>
-                     </div>
-
-                   </div>
+                    </div>
+                  </div>
                  </div>';
                }
 
@@ -388,9 +390,11 @@
          <?php
 
           $nbrArray = Work::getCntArticle() / 12;
-          for ($i = 1; $i < $nbrArray+1; $i++) {
-            echo '<a href="#sample" onclick="viewPage('.$i.');">'.$i.'</a>';
-            if ($i <$nbrArray) echo ' - ';
+          if ($nbrArray > 0) {
+            for ($i = 1; $i < $nbrArray+1; $i++) {
+              echo '<a href="#sample" onclick="viewPage('.$i.');">'.$i.'</a>';
+              if ($i <$nbrArray) echo ' - ';
+            }
           }
 
          ?>
