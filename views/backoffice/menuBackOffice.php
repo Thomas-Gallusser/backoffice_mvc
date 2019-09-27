d811f503454c35389799baa83280358f6f9cf424
Thomas-Gallusser <t.gallusser@codeur.online>
2019-09-27 12:31:09 +0200

    fixe merge

--- views/backoffice/menuBackOffice.php
+++ views/backoffice/menuBackOffice.php
@@ -4,15 +4,9 @@
   <button class="dropdown-btn active">Articles
     <i class="fa fa-caret-down"></i>
   </button>
-<<<<<<< HEAD
   <div class="dropdown-container" style="display:none">
     <a href="?backoffice&type=1&see&p=1">Voir tous les articles</a>
     <a href="?backoffice&type=1&add">Ajouter un article</a>
-=======
-  <div class="dropdown-container" style="display:block">
-    <a href="index.php?backoffice&type=1&see=1&p=1">Voir tous les articles</a>
-    <a href="index.php?backoffice&type=1&add=1">Ajouter un article</a>
->>>>>>> 835437119caa134ed11f222e8b9b2d58a7876804
   </div>
 
   <button class="dropdown-btn">Gestion d'utilisateurs
